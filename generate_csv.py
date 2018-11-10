2191f9f8a52b53dc9c91a47f765ea6964f05e0df
ergosteur <matt.yiptong@gmail.com>
2018-11-10 16:36:36 -0500

    CSV fixes quote csv fix standalone json -> CSV script

--- generate_csv.py
+++ generate_csv.py
@@ -1,19 +1,14 @@
-import tweepy
 import json
-import math
-import glob
 import csv
-import zipfile
-import zlib
-from tweepy import TweepError
-from time import sleep
+
 user = 'gibiofficial'
 user = user.lower()
-with open('gibiofficial_short.json') as master_file:
+with open('data/gibiofficial_short.json') as master_file:
     data = json.load(master_file)
     fields = ["favorite_count", "source", "text", "in_reply_to_screen_name", "is_retweet", "created_at", "retweet_count", "id_str"]
     print('creating CSV version of minimized json master file')
-    f = csv.writer(open('gibiofficial2.csv', 'w', quotechar='"', encoding='utf-8'))
+    csvFile = open('data/gibiofficial2.csv'.format(user), 'w', newline='', encoding='utf-8')
+    f = csv.writer(csvFile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
     f.writerow(fields)
     for x in data:
         f.writerow([x["favorite_count"], x["source"], x["text"], x["in_reply_to_screen_name"], x["is_retweet"], x["created_at"], x["retweet_count"], x["id_str"]])
